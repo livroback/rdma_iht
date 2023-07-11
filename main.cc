c2755e96359351c0b774038c1705db11df98a42a
livroback <ojr224@lehigh.edu>
2023-07-11 16:30:45 -0400

    linked list started

--- main.cc
+++ main.cc
@@ -180,7 +180,7 @@ int main(int argc, char **argv)
         std::this_thread::sleep_for(std::chrono::milliseconds(200)); // So we get this printed last
         ROME_INFO("Not in experiment. Shutting down");
         return 0;
-    }
+    } 
 
     // Make a memory pool for the node to share among all client instances
     uint32_t block_size = 1 << params.region_size();
