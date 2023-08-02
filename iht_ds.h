5e33b1c3cbb109da352a35de11aa2d57fb88380f
livroback <ojr224@lehigh.edu>
2023-08-02 10:07:43 -0400

    zoom call push

--- iht_ds.h
+++ iht_ds.h
@@ -16,17 +16,21 @@ using ::rome::rdma::remote_nullptr;
 using ::rome::rdma::remote_ptr;
 using ::rome::rdma::RemoteObjectProto;
 
-class Node {
+
+static int length; 
+
+
+class LinkedList {
+
+
 public:
+    struct Node {
     int data;
     remote_ptr<Node> next;
 
     Node() : data(0), next(remote_nullptr) {}
     Node(int d) : data(d), next(remote_nullptr) {}
 };
-
-class LinkedList {
-public:
 
     remote_ptr<Node> head;
 
@@ -72,9 +76,9 @@ public:
 
             // Init plist and set remote proto to communicate its value
             InitLinkedList(ll_root);
+             printf("init done");
 
             this->root = ll_root;
-            // this->head = ll_root->head; //try this?? 
 
             proto.set_raddr(ll_root.address());
 
@@ -91,24 +95,7 @@ public:
                 absl::Status status = conn_or.value()->channel()->Send(proto);
                 ROME_CHECK_OK(ROME_RETURN(status), status);
             }
-
-        } else {
-            // Listen for a connection
-            auto conn_or = pool_->connection_manager()->GetConnection(host.id);
-            ROME_CHECK_OK(ROME_RETURN(conn_or.status()), conn_or);
-
-            // Try to get the data from the machine, repeatedly trying until successful
-            auto got = conn_or.value()->channel()->TryDeliver<RemoteObjectProto>();
-            while (got.status().code() == absl::StatusCode::kUnavailable) {
-                got = conn_or.value()->channel()->TryDeliver<RemoteObjectProto>();
-            }
-            ROME_CHECK_OK(ROME_RETURN(got.status()), got);
-
-            // From there, decode the data into a value
-            remote_ptr<LinkedList> ll_root = decltype(ll_root)(host.id, got->raddr());
-            this->root = ll_root;
-        }
-
+        } 
         return absl::OkStatus();
     }
 };
@@ -116,57 +103,92 @@ public:
 void LinkedList::InitLinkedList(remote_ptr<LinkedList> p) {
     ROME_INFO("Running the init linked list function");
 
-    // Allocate memory for the head if it's null
-    if (is_null(p->head)) {
+    // Allocate memory for the head to no longer make it null
         p->head = pool_->Allocate<Node>();
-        ROME_INFO("Head is allocated");
-
-        // Use RDMA write to set the value of the head in remote memory
-        pool_->Write<Node>(remote_nullptr, *(p->head)); 
-    }
+        length = -1; 
+        length++;  
+        printf("LENGTH = %d\n", length); 
+        head = p->head; // [ojr] just trying this .....
+    
+    //    [ojr] This line gives me a remote access error (Does allocate automatically make it a remote_nullptr ?) (it does not)  
+    //    pool_->Write<Node>(remote_nullptr, *(p->head)); 
 }
 
 
 void LinkedList::insertNode(int d) {
-    // Create the new node to insert
-    remote_ptr<Node> nodeToAdd = pool_->Allocate<Node>();
 
-    // [ojr] These next two lines???? Do they need a pool_Write? Not sure. 
-    nodeToAdd->data = d;
-    pool_->Write<Node>(remote_nullptr, *(nodeToAdd->next)); 
-    // nodeToAdd->next = remote_nullptr; (Replaced with the line above)
-    
-    if (is_null(head)) {
-        ROME_INFO("Head is a null!");
-        // [ojr] Pool_write for this next line?? 
-        pool_->Write<Node>(nodeToAdd, *head); 
-        // head = nodeToAdd; (Replaced with the line above)
+    //---------------------------
+    //[ojr] Writing the next node as a null ends up in a seg fault so do this at the end ???? 
+    // pool_->Write<Node>(remote_nullptr, *(head->next)); 
+    // nodeToAdd->next = remote_nullptr; //(Replaced with the line above)
+    //---------------------------
+
+
+    //[ojr] At this point, we have allocated a head but it is not the new node 
+    if (length == 0) {
+
+            // Create the new node to insert
+        remote_ptr<Node> nodeToAdd = pool_->Allocate<Node>();
+        nodeToAdd->data = d;
+
+        ROME_INFO("Node {} getting added to head....", d);
+
+         pool_->Write<Node>(head, *(nodeToAdd)); 
+
+         // [ojr] Instead of last val pointing to null, point to a dummy value or maybe just use a length var? Nul
+
+        length++;         
         ROME_INFO("Node {} has been added to head", d);
-    } else {
-        // Start at pointer to the head and iterate to the last node in the list
+    } 
+    else {
+
+         // Create the new node to insert
+        remote_ptr<Node> nodeToAdd = pool_->Allocate<Node>();
+        nodeToAdd->data = d;
+        
+        ROME_INFO("Node {} getting added to end of list....", d);
+
+
+
+
+
+        
+       
         remote_ptr<Node> c = pool_->Read<Node>(head);
+  
+        printf("Value at head is %d\n", c->data); 
 
-        // Iterate to the last node in the list
-        while (!is_null(c->next)) {
-            c = pool_->Read<Node>(c->next);
-            ROME_INFO("Value at current is = {}", c->data);
-        }
+        //[ojr] still need to find a way how to update the pointers to iterate through list 
 
 
-        // c->next = nodeToAdd;
-        pool_->Write<Node>(nodeToAdd, *(c->next)); 
+            if(is_null(c->next)){ //[ojr] this code only used for 2 nodes in list ^^ (just testing)
+                printf("next is null\n");
+                c->next = pool_->Allocate<Node>();
+                pool_->Write<Node>(c->next, *(nodeToAdd));  
+            }
+        
 
-        //[ojr] replacing line 151 with this  pool_->Write<Node>(nodeToAdd, *(c->next));   = seg fault 
+        printf("Value at head->next is %d\n", c->next->data);
+ 
 
-        ROME_INFO("c-> next = {}", c->next->data);
+    //----------------------------------------------------
 
-        //[ojr] doing a pool_->Write with this also gives me a seg fault 
-        // nodeToAdd->next = remote_nullptr;
+        // //[ojr] replacing line 151 with this  pool_->Write<Node>(nodeToAdd, *(c->next));   = seg fault 
 
-        pool_->Write<Node>(remote_nullptr, *(nodeToAdd->next)); 
+        // //[ojr] doing a pool_->Write with this also gives me a seg fault 
+        // // nodeToAdd->next = remote_nullptr;
+
+        // pool_->Write<Node>(remote_nullptr, *(nodeToAdd->next)); 
+
+        //-------------------------
+
+        length++; 
+        printf("LENGTH (END OF LIST) = %d\n", length); 
+        
+        ROME_INFO("Node {} has been added to end of the list", d);
 
     }
-        ROME_INFO("Node {} has been added to end of the list", d);
+       
 }
 
 
@@ -216,17 +238,42 @@ void LinkedList::insertNode(int d) {
 
 void LinkedList::printList() {
 
+    //[ojr] this code gives a remote access error even when we only have 5->2 -> null as our list 
+    //[ojr] just wrote this to test if head->next is there(line 239) gives the error 
     remote_ptr<Node> t = pool_->Read<Node>(head); 
-    while (!is_null(t)) {
-      printf("%d -> ", t->data);
-      //To not seg fault -> cannot do pool_->Read with a null pointer 
-      if(is_null(t->next)){
-        break;
-      }
-      else{
-        t = pool_->Read<Node>(t->next);
-      }
-    }
+              printf("%d -> ", t->data);
+    remote_ptr<Node> x = pool_->Read<Node>(head->next); 
+                  printf("%d -> ", x->data);
+
+//------------------------------------------------------
+
+
+
+
+
+    // for(int i=0; i<length; i++){
+    //       printf("%d -> ", t->data);
+    //           t = pool_->Read<Node>(t->next);
+    //   //To not seg fault -> cannot do pool_->Read with a null pointer 
+    //   if(is_null(t->next)){
+    //     printf("boom");
+    //     break;
+    //   }
+    //   else{
+    //     t = pool_->Read<Node>(t->next);
+    //   }
+
+    // }
+    // while (!is_null(t)) {
+    //   printf("%d -> ", t->data);
+    //   //To not seg fault -> cannot do pool_->Read with a null pointer 
+    //   if(is_null(t->next)){
+    //     break;
+    //   }
+    //   else{
+    //     t = pool_->Read<Node>(t->next);
+    //   }
+    // }
     printf("NULL");
     printf("\n");
 }
